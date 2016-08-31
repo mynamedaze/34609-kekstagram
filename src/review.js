914db0d0d6a3527e7fc1b1722d29ce8e7da42ebc
Murat Kudainetov <murat.kudainetov@gmail.com>
2016-09-01 02:11:07 +0300

    5.1. Модули. Разбил на модули с использованием CommonJS

---
+++ src/review.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var filtersForm = document.querySelector('.filters');
+var IMAGE_LOAD_TIMEOUT = 3000;
+var templateElement = document.querySelector('template');
+var container = document.querySelector('.container');
+
+module.exports = function(picture) {
+
+  var elementToClone;
+  var backgroundLoadTimeout;
+
+  if('content' in templateElement) {
+    elementToClone = templateElement.content.querySelector('.picture');
+  } else {
+    elementToClone = templateElement.querySelector('.picture');
+  }
+
+  var element = elementToClone.cloneNode(true);
+  var img = element.querySelector('img');
+
+  backgroundLoadTimeout = setTimeout(function() {
+    img.src = '';
+    element.classList.add('picture-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+
+  img.onload = function() {
+    clearTimeout(backgroundLoadTimeout);
+  };
+
+  img.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
+
+  img.src = picture.url;
+  container.appendChild(element);
+
+  return element;
+};
+
+if(filtersForm.classList.contains('hidden')) {
+  filtersForm.classList.remove('hidden');
+}
