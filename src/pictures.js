a58b5a00caf9957dd27591e3ec5c142d51350f4c
Murat Kudainetov <murat.kudainetov@gmail.com>
2016-09-01 20:32:29 +0300

    исправлена отправка коммита

--- src/pictures.js
+++ src/pictures.js
@@ -5,6 +5,7 @@ var loadJSONPData = require('./load');
 var generatePicture = require('./review');
 var filtersForm = document.querySelector('.filters');
 
+filtersForm.classList.add('hidden');
 loadJSONPData('http://localhost:1506/api/pictures?callback=', function(data) {
   data.forEach(function(picture) {
     generatePicture(picture);
