17c45724255e736e95681f9727af713e04faa86e
Murat Kudainetov <murat.kudainetov@gmail.com>
2016-09-04 12:58:55 +0300

    Gallery. 6.1. Поправил review.js на picture.js. Добавил обработчик клика.

--- src/pictures.js
+++ src/pictures.js
@@ -2,14 +2,16 @@
 
 
 var loadJSONPData = require('./load');
-var generatePicture = require('./review');
+var generatePicture = require('./picture');
 var gallery = require('./gallery');
 var filtersForm = document.querySelector('.filters');
 
 filtersForm.classList.add('hidden');
 loadJSONPData('http://localhost:1506/api/pictures?callback=', function(data) {
-  data.forEach(function(picture) {
-    generatePicture(picture);
+  data.forEach(function(picture, index) {
+    generatePicture(picture, index);
   });
+
+  gallery.setPictures(data);
   filtersForm.classList.remove('hidden');
 });
